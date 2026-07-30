6ac9f17bbd3fd331432e16610735836e88ec67ae
bhavyachhabra65@gmail.com <bhavyachhabra65@gmail.com>
2026-07-30 15:08:14 +0530

    feat: HDFC Cheque Coordintes changed

--- apps/cheque/layouts/hdfc.py
+++ apps/cheque/layouts/hdfc.py
@@ -1,9 +1,9 @@
-DATE = {"top": 9.8, "left": 155.0}
+DATE = {"top": 7.7, "left": 147.0}
 
-PAY_TO = {"top": 24.7, "left": 20.0}
+PAY_TO = {"top": 23.2, "left": 20.0}
 
-AMOUNT_WORDS = {"top": 32.7, "left": 30.0}
+AMOUNT_WORDS = {"top": 30.0, "left": 28.0}
 
-AMOUNT = {"top": 39.7, "left": 157.0}
+AMOUNT = {"top": 35.0, "left": 150.0}
 
 AC_PAYEE_ONLY = {"top": 12.8, "left": 100.0}
