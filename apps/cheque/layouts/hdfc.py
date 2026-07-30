5e9daf10aa4ae0b753e0f008485a755feacea6c1
bhavyachhabra65@gmail.com <bhavyachhabra65@gmail.com>
2026-07-30 19:38:57 +0530

    feat: ICICI Cheque Coordintes changed and little bit HDFC

--- apps/cheque/layouts/hdfc.py
+++ apps/cheque/layouts/hdfc.py
@@ -1,9 +1,9 @@
-DATE = {"top": 7.7, "left": 147.0}
+DATE = {"top": 6.7, "left": 147.0}
 
-PAY_TO = {"top": 23.2, "left": 20.0}
+PAY_TO = {"top": 20.0, "left": 20.0}
 
-AMOUNT_WORDS = {"top": 30.0, "left": 28.0}
+AMOUNT_WORDS = {"top": 29.0, "left": 28.0}
 
-AMOUNT = {"top": 35.0, "left": 150.0}
+AMOUNT = {"top": 34.0, "left": 150.0}
 
-AC_PAYEE_ONLY = {"top": 12.8, "left": 100.0}
+AC_PAYEE_ONLY = {"top": 12.5, "left": 100.0}
--- apps/cheque/layouts/icici.py
+++ apps/cheque/layouts/icici.py
@@ -1,9 +1,9 @@
-DATE = {"top": 14.5, "left": 147.0}
+DATE = {"top": 8.0, "left": 145.0}
 
-PAY_TO = {"top": 29.0, "left": 18.0}
+PAY_TO = {"top": 22.0, "left": 15.0}
 
-AMOUNT_WORDS = {"top": 42.0, "left": 18.0}
+AMOUNT_WORDS = {"top": 29.0, "left": 22.0}
 
-AMOUNT = {"top": 42.0, "left": 149.0}
+AMOUNT = {"top": 36.0, "left": 152.0}
 
-AC_PAYEE_ONLY = {"top": 8.0, "left": 22.0}
+AC_PAYEE_ONLY = {"top": 4.0, "left": 115.0}
